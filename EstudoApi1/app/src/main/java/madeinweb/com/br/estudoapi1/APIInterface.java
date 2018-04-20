9629f8ca87d43abd67467f72bc02c41d97de8f0a
roger8b <roger.8b@gmail.com>
2018-04-19 23:00:04 -0300

    Inicial

---
+++ EstudoApi1/app/src/main/java/madeinweb/com/br/estudoapi1/APIInterface.java
@@ -0,0 +1,32 @@
+package madeinweb.com.br.estudoapi1;
+
+import madeinweb.com.br.estudoapi1.pojo.MultipleResource;
+import madeinweb.com.br.estudoapi1.pojo.User;
+import madeinweb.com.br.estudoapi1.pojo.UserList;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+/**
+ * Created by anupamchugh on 09/01/17.
+ */
+
+interface APIInterface {
+
+    @GET("/api/unknown")
+    Call<MultipleResource> doGetListResources();
+
+    @POST("/api/users")
+    Call<User> createUser(@Body User user);
+
+    @GET("/api/users?")
+    Call<UserList> doGetUserList(@Query("page") String page);
+
+    @FormUrlEncoded
+    @POST("/api/users?")
+    Call<UserList> doCreateUserWithField(@Field("name") String name, @Field("job") String job);
+}
